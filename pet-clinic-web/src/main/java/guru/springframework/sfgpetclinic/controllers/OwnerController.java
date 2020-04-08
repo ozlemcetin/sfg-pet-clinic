4012925339f54c186d4e7d72cbbd0af02f169104
ozlem <ozlem.cetin@gmail.com>
2020-04-08 22:10:29 +0300

    Menu Links are Broken closes #45

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -15,11 +15,16 @@ public class OwnerController {
         this.ownerService = ownerService;
     }
 
-    @RequestMapping({ "", "/", "/index", "/index.html"})
-    public String listOwners(Model model){
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listOwners(Model model) {
 
         model.addAttribute("owners", ownerService.findAll());
 
         return "owners/index";
+    }
+
+    @RequestMapping("/find")
+    public String findOwner() {
+        return "notImplemented";
     }
 }

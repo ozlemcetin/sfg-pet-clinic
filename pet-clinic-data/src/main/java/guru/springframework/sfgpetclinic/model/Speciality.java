a557d3770285e38087c6e5d34792089e6200d385
ozlem <ozlem.cetin@gmail.com>
2020-04-11 17:08:51 +0300

    Converts Vets to JPA Entities Closes #37

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
@@ -1,9 +1,20 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity {
 
     private String description;
 
+    //@ManyToMany(mappedBy = "specialties")
+    //private Set<Vet> vets = new HashSet<>();
+
+    /*
+     Getters and Setters
+     */
     public String getDescription() {
         return description;
     }
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,16 +1,27 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
-    private Set<Speciality> specialties;
+    /*
+    By deafult, ManyToMany relationships LAZILY initialized.
+    By EAGER, JPA tries to load everything all at once
+     */
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vets_specialities",
+            joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "speciality_id"))
+    private Set<Speciality> specialties = new HashSet<>();
 
-    public Vet() {
-        specialties = new HashSet<>();
-    }
 
+    /*
+     Getters and Setters
+     */
     public Set<Speciality> getSpecialties() {
         return specialties;
     }
